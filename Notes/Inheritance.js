739680564abc1106447bf5690e0d02a7b5203150
anshu-goyal-lf <anshu.goyal@loanframe.com>
2018-03-18 14:28:55 +0530

    Contract Created

---
+++ Notes/Inheritance.js
@@ -0,0 +1,11 @@
+
+1. 'extend' keyword is replaced by 'is'.
+2. 'protected' -> 'internal'
+3.  There is no keyword 'abstract' in solidity.
+4.  accessModifier use. We can create our own accessModifier.
+
+    Error handling:
+
+4.  'assert', 'require' and 'revert' functionalities. Throw is getting deprecated. This is related to
+    gas consumption.
+5.  'fallback' function is function without any name.
